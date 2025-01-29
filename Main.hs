57378e248b8beb69cb4d7bf69a2000b450d609d4
Victor H. Manotas G. <victorma31@gmail.com>
2025-01-29 16:07:18 -0500

    Setup exercise 2

--- Main.hs
+++ Main.hs
@@ -1,6 +1,11 @@
 module Main where
 import System.Environment
-
+{{-
+Exercise 2 (TODO): Change the program so it performs a simple arithmetic operation on the
+two arguments and prints out the result. You can use read to convert a
+string to a number, and show to convert a number back into a string. Play
+around with different operations.
+-}}
 main :: IO ()
 main = do args <- getArgs
           putStrLn ("Hello, " ++ head args)
