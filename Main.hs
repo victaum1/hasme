13486f1c7d1b04117b17d0b8100929f82cadbe5f
Victor H. Manotas G. <victorma31@gmail.com>
2025-01-29 16:11:02 -0500

    Setup exercise 3

--- Main.hs
+++ Main.hs
@@ -1,6 +1,10 @@
 module Main where
 import System.Environment
-
+{{-
+Exercise 3 (TODO): getLine is an IO action that reads a line from the console and returns
+it as a string. Change the program so it prompts for a name, reads the
+name, and then prints that instead of the command line value.
+-}}
 main :: IO ()
 main = do args <- getArgs
           putStrLn ("Hello, " ++ head args)
