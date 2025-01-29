6c3f42fdb39cbae1b9b295acd7e59c8e15e9a980
Victor H. Manotas G. <victorma31@gmail.com>
2025-01-29 15:51:31 -0500

    Setup exercise 1

--- Main.hs
+++ Main.hs
@@ -1,5 +1,9 @@
 module Main where
 import System.Environment
+{{-
+Exercise 1 (TODO): Change the program so it reads two arguments from the command line,
+and prints out a message using both of them. 
+-}}
 
 main :: IO ()
 main = do args <- getArgs
